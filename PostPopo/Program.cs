5d0b227c9ebd6aba0acaa15d6446e07ec912ddf5
Jimmy <jimmy@ceis.tw>
2022-07-28 17:22:07 +0800

    加入計時器 每次呼叫API 間隔500毫秒

--- PostPopo/Program.cs
+++ PostPopo/Program.cs
@@ -3,7 +3,9 @@ using System.ComponentModel.DataAnnotations;
 using System.Net.Http.Headers;
 using System.Text;
 using System.Text.Json.Serialization;
+using System.Diagnostics;
 
+var sw = new Stopwatch(); sw.Start();
 var client = new HttpClient();
 var url= "https://inline.app/waiting/api/waiting/create-waiting?company=-MNCESUEwk6XYGi_SSev%3Ainline-live-2&branch=-MNCESeVk25UUKomalfj";
 var request = new
@@ -28,9 +30,12 @@ while (!responseMessage.IsSuccessStatusCode)
     PostTimes++;
     Console.WriteLine(responseMessage.StatusCode);
     Console.WriteLine($"呼叫API第{PostTimes}次");
+    Thread.Sleep(500);
     responseMessage = await client.PostAsync(url, content);
 }
-Console.WriteLine($"呼叫API成功");
+sw.Stop();
+Console.WriteLine($"執行花了 {sw.Elapsed.TotalSeconds} 秒鐘");
+Console.WriteLine($"{DateTime.Now}呼叫API成功");
 Console.WriteLine(responseMessage.StatusCode);
 Console.WriteLine(responseMessage.Content);
 byte[] buffer = await responseMessage.Content.ReadAsByteArrayAsync();
